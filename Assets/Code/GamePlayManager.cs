4a7b07f9dac4ba8506c04440d43c678f8dde5e27
Flame260 <162753205+Flame260@users.noreply.github.com>
2026-09-25 02:07:24 -0400

    Random

--- Assets/Code/GamePlayManager.cs
+++ Assets/Code/GamePlayManager.cs
@@ -1,56 +1,107 @@
 using UnityEngine;
 using TMPro;
 using DG.Tweening;
+using Unity.VisualScripting;
 
-
-public class GamePlayManager : MonoBehaviour
+public class GameplayManagerScript : MonoBehaviour
 {
-    public enum MoveType{
+    public enum MoveType
+    {
         MoveDistance,
         MoveToTarget,
         MoveToClick
     }
-    
+
+    [Header("Movement")]
     public MoveType moveType;
-    public float MoveDistance=2f;
-    public float moveTime=0.5f
+
+    public float moveDistance = 2f;
+    public float moveTime = 0.5f;
 
     public Transform targetPosition;
 
+    [Header("Click Movement")]
     public Camera mainCamera;
 
+    [Header("Score")]
     public TextMeshProUGUI scoreText;
     public int score = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       UpdateScore(); 
+        UpdateScore();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        //spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Move();
+        }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (moveType == MoveType.MoveToClick)
+            {
+                MoveToClickedPosition(Input.mousePosition);
+            }
+            else
+            {
+                Move();
+            }
         }
     }
 
-    void UpdateScore(){
+    void Move()
+    {
+        //distance forward
+        if (moveType == MoveType.MoveDistance)
+        {
+            Vector3 newPosition = transform.position + transform.forward * moveDistance;
+            transform.DOMove(newPosition, moveTime);
+        }
+
+        //move to gameobject
+        if (moveType == MoveType.MoveToTarget)
+        {
+            transform.DOMove(targetPosition.position, moveTime);
+        }
+    }
+
+    void MoveToClickedPosition(Vector2 screenPosition)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 newPosition = hit.point;
+            newPosition.z = transform.position.z;
+            transform.DOMove(newPosition, moveTime).SetEase(Ease.InOutQuad);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            UpdateScore();
+        }
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+
+    void UpdateScore()
+    {
         scoreText.text = score.ToString();
     }
-    void MovetoPosition(){
-
-    }
-    
-    void Move(){
-        if(moveType == MoveType.MoveToTarget){
-            Vector3 newPosition=transform.position+transform.forward*MoveDistance;
-            transform.DOMove(newPosition=moveTime)
-        }
-    }
-
-    public void MoveToClickPosition(Vector3 screenPosition){
-
-    }
-    
 }
---
+++ Assets/Code/SceneChange.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
